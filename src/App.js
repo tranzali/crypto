9c913a69c0a384858d98e33b69ddd0fc9f8a9266
M Ali <ma_uk@hotmail.com>
2022-03-09 14:46:57 +0000

    api fetch and data display complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import { useEffect, useState } from 'react';
+import Axios from 'axios';
 import './App.css';
 
 function App() {
+  const [crypto, setCrypto] = useState([]);
+
+  useEffect(() => {
+    // https://documenter.getpostman.com/view/5734027/RzZ6Hzr3 for api 
+    Axios.get("https://api.coinstats.app/public/v1/coins?skip=0&limit=10")
+      .then((res) => {
+        setCrypto(res.data.coins);
+      })
+
+  },[])
+
   return (
     <div className="App">
-      <h1>Working</h1>
+      <div className="cryptoHead"></div>
+      <div className="cryptoList">
+      {crypto.map((coin) => {
+        return <h1 key={coin.id}>{coin.name}</h1>
+      })}
+      </div>
     </div>
   );
 }
