64e89990728b0cca1c1b9d26e2fab05a4d05224f
M Ali <ma_uk@hotmail.com>
2022-03-14 17:51:05 +0000

    app finished

--- src/App.js
+++ src/App.js
@@ -5,21 +5,36 @@ import Coin from './component/Coin';
 
 function App() {
   const [crypto, setCrypto] = useState([]);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     // https://documenter.getpostman.com/view/5734027/RzZ6Hzr3 for api 
-    Axios.get("https://api.coinstats.app/public/v1/coins?skip=0&limit=10")
+    Axios.get("https://api.coinstats.app/public/v1/coins?skip=0")
       .then((res) => {
         setCrypto(res.data.coins);
       })
 
   },[])
 
+  const filteredList = crypto.filter((coin) => {
+    return coin.name.includes({search})
+    
+  })
+
+
   return (
     <div className="App">
-      <div className="cryptoHead"></div>
+      <div className="cryptoHead">
+        <input 
+          type="search" 
+          placeholder='Bitcoin...' 
+          onChange={(event) => {
+            setSearch(event.target.value)
+          }} 
+        />
+      </div>
       <div className="cryptoList">
-      {crypto.map((coin) => {
+      {filteredList.map((coin) => {
         return (
           <Coin 
             key={coin.id.toString()}
